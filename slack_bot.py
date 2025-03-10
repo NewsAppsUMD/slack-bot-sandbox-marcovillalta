777df09cdb33fe7e8c45fdb54a7ffbe4f81e98cc
Marco Villalta <157721934+marcovillalta@users.noreply.github.com>
2025-03-10 20:09:06 +0000

    slackbot setup

--- slack_bot.py
+++ slack_bot.py
@@ -5,7 +5,7 @@ from slack.errors import SlackApiError
 slack_token = os.environ.get('SLACK_API_TOKEN')
 
 client = WebClient(token=slack_token)
-msg = "testing!"
+msg = "Cowboys suck and will never see a Super Bowl again. #Niners"
 try:
     response = client.chat_postMessage(
         channel="slack-bots",
